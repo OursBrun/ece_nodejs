1808d132dcc34531216f4b482c8757435049019c
Clement Camboulives <cambouli@ece.fr>
2014-11-16 20:22:43 +0100

    Add Jade

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var app = express();
+var jade = require('jade');
+
+var fn = jade.compile('
+doctype html
+html(lang="fr")
+  head
+    title= Hello World
+  body
+    h1 Hello World
+    #test.yolo
+      p.
+        Jade c\'est vraiment tout moche a utiliser...
+')
+
+var html = fn();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.send(html);
 });
 
 app.listen(3000);
